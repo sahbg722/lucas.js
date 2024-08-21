f90ba2f1421d53a262f3e228be610e14f0bd19ca
sahbg722 <144184809+sahbg722@users.noreply.github.com>
2024-08-20 21:49:14 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,31 @@
+var nome = "sabrina" 
+console.log(nome)
+
+//var idade = "18" 
+//console.log(idade)
+
+let idade = 20
+console.log(idade)
+
+const cpf = 15975344498
+console.log(" Meu cpf:" + cpf)
+
+const adicao = 20 + 10
+console.log(" A soma é:" + adicao)
+
+var numero1 = 10
+var numero2 = 20
+var soma = numero1 + numero2
+console.log("A soma é: " + soma)
+
+let booleana = true //or false
+console.log(booleana)
+
+// lista
+let lista = [1,2,3,4,5];
+console.log(lista)
+
+//objeto
+let person = {nome: "sabrina",
+            idade: 20}
+console.log(person)
